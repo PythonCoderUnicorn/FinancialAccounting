e0e2ce8a5bdbe034ecd22952101d27d42c04f0c5
Zane Dax <neptunium0237@gmail.com>
2022-10-18 10:22:24 -0600

    dupont equation

--- documents/dupont.py
+++ documents/dupont.py
@@ -1,69 +1,18 @@
 class Dupont_equation:
-"""
-    def __init__(self, dict_, list_) -> None:
-        self.dict_ = dict_ 
-        self.list_ = list_ 
     
-    def profit_margin(self,dict_):
-        assets_list = list(self.dict_.values())
-
-        profit_margin = assets_list[0] / assets_list[1]
-        answers.append(profit_margin)
-        print("Profit margin = $ {:,.2f}".format(profit_margin))
-
-
-    def avg_total_assets( self, dict_):
-        assets_list = list(self.dict_.values())
-
-        avg_tot_assets = (assets_list[2] + assets_list[3] ) / 2
-        answers.append(avg_tot_assets)
-        print("Average total assets = $ {:.2f}".format(avg_tot_assets))
-
-
-    def asset_turnover( self, dict_, list_):
-        assets_list = list(self.dict_.values())
-
-        avg_tot_assets = (assets_list[2] + assets_list[3] ) / 2
-
-        turnover = assets_list[1] / avg_tot_assets
-
-        self.list_.append(turnover)
-        
-        print("The asset turnover = $ {:.2f}".format(turnover))
-
-
-    def financial_leverage( self,dict_, list_ ):
-        assets_list = list(self.dict_.values())
-
-        fin_lev = assets_list[3] / assets_list[4]
-
-        self.list_.append(fin_lev)
-        print("Financial leverage = $ {:.2f}".format(fin_lev))
-
-
-    def return_on_equity(self,answers):
-        roe = answers[0] * answers[2] * answers[3]
-        print("Return on equity = {:.2f}%".format(roe * 100))
-        
-        
-        
-        
-pl = {
+"""      
+profit_loss = {
 'net_income':185,
 'net_sales':2628
 }
 
-bs = {
+balance_sheet = {
     'beginning_assets':5954,
     'ending_assets':4601,
     'ending_equity':1105,
 }
 
-roe = dupont_equation(pl,bs)
-print(print_percentage(roe))
-
-
- """
+"""
     def make_profit_margin(net_income,net_sales):
         profit_margin = net_income/net_sales
         return profit_margin
@@ -96,4 +45,4 @@ print(print_percentage(roe))
         asset_turnover = make_asset_turnover(pl['net_sales'], average_total_assets)
         financial_leverage = make_financial_leverage(bs['ending_assets'], bs['ending_equity'])
         return_on_equity = make_return_on_equity(profit_margin, asset_turnover, financial_leverage)
-        return return_on_equity
+        # return return_on_equity
